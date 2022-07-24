daebe369268d894ae7280592ca8fb37ca103b451
patkamon <patkamontura@gmail.com>
2022-07-24 17:56:32 +0700

    add nuget package

--- Play.Catalog.Service/Startup.cs
+++ Play.Catalog.Service/Startup.cs
@@ -1,22 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization;
-using MongoDB.Bson.Serialization.Serializers;
-using MongoDB.Driver;
-using Play.Catalog.Service.Repositories;
-using Play.Catalog.Service.Settings;
+using Play.Catalog.Service.Entities;
+using Play.Common;
+using Play.Common.Settings;
+
 
 namespace Play.Catalog.Service
 {
@@ -34,21 +25,10 @@ namespace Play.Catalog.Service
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
-            BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
-            BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
-
             serviceSetting = Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
-
-            services.AddSingleton(ServiceProvider =>
-            {
-                var mongoDbSetting = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-                var mongoClient = new MongoClient(mongoDbSetting.ConnectionString);
-                return mongoClient.GetDatabase(serviceSetting.ServiceName);
-            });
             
-            services.AddSingleton<IItemsRepository, ItemsRepository>();
-            
+            services.AddMongo().AddMongoRepository<Item>("items");
+           
             services.AddControllers(options =>
             {
                 options.SuppressAsyncSuffixInActionNames = false;
